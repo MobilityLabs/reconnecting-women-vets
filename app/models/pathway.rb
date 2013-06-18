14a0b55bf01abcdbf30ef7affe8dd1ea5a0d6a47
Marlon Nelson <marlon.nelson@gmail.com>
2013-06-18 15:16:07 -0400

    switch to scope block to remove deprecation warning for question ordering

--- app/models/pathway.rb
+++ app/models/pathway.rb
@@ -1,6 +1,6 @@
 class Pathway < ActiveRecord::Base
 
-  has_many :questions, order: :order, dependent: :destroy
+  has_many :questions, -> { order :order }, dependent: :destroy
   has_and_belongs_to_many :action
 
   accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:text].blank? }, :allow_destroy => true
