ed3db7309a368c66bbc65450754c84f9ca8629ee
Marlon Nelson <marlon.nelson@gmail.com>
2013-06-18 11:23:53 -0400

    Remove local ruby version requirement from deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require "rvm/capistrano"
 
-set :rvm_ruby_string, :local               # use the same ruby as used locally for deployment
+# set :rvm_ruby_string, :local               # use the same ruby as used locally for deployment
 set :rvm_autolibs_flag, "read-only"        # more info: rvm help autolibs
 
 # before 'deploy:setup', 'rvm:install_rvm'   # install RVM
