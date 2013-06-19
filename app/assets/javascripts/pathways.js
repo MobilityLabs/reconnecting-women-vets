cff5fe4e8c72a0e582536955c11f68eafbf34f51
Marlon Nelson <marlon.nelson@gmail.com>
2013-06-19 08:33:38 -0400

    pathway megaform adds multiple questions properly

--- app/assets/javascripts/pathways.js
+++ app/assets/javascripts/pathways.js
@@ -1,9 +1,24 @@
 $(function(){
+
+  // Clone the template question to be used as a new template question
+  // Convert the original template question into a new question,
+  //   with a handler for the remove button
+  // Update the new template's id and name and append it to the questions
   $('button#add_question').click(function(){
-    var $q = $('.question.template').clone().removeClass('template');
-    $q.find('button.remove_question').click(function(e){
+    var $q = $('.question.template'),
+        $qt = $q.clone(), $qt_id = $qt.attr('id'),
+        $qt_input = $qt.find('input'),
+        $qt_label = $qt.find('label'),
+        seq = $('.question').size();
+
+    $q.removeClass('template').find('button.remove_question').click(function(e){
       $(e.target).parents('.question').remove();
     });
-    $q.appendTo('#questions');
+
+    $qt_input
+      .attr('id', 'pathway_questions_attributes_'+seq+'_text')
+      .attr('name', 'pathway[questions_attributes]['+seq+'][text]');
+    $qt_label.attr('for', $qt_input.attr('id'));
+    $qt.appendTo('#questions');
   });
 });
