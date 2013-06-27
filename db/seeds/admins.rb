91c180f3367d619b590e5f0428fb4fae64420bed
Marlon Nelson <marlon.nelson@gmail.com>
2013-06-27 11:18:44 -0400

    update admins seeds

--- db/seeds/admins.rb
+++ db/seeds/admins.rb
@@ -1,9 +1,11 @@
-[{email:'sean@mobility-labs.com', password:'PaSsWoRd'},
- {email:'marlon.nelson@gmail.com', password:'test1234'},
- {email:'test@localhost.com', password:'test1234'}].each do |seed|
+[{email:'sean@mobility-labs.com', password:'PaSsWoRd', confirm: true},
+ {email:'marlon.nelson@gmail.com', password:'test1234', confirm: true},
+ {email:'testadmin@localhost.com', password:'test1234', confirm: true},
+ {email:'test@localhost.com', password:'test1234', confirm: false}].each do |seed|
   admin = Admin.find_or_initialize_by(email: seed[:email])
   if admin.new_record?
     admin.password_confirmation = admin.password = seed[:password]
     admin.save!
+    admin.confirm! if seed[:confirm]
   end
 end
